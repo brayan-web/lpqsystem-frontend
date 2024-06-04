f849b538fa12f8fdd3da32da617396a24946d3a3
brayan <brayan.mail.tester@gmail.com>
2024-06-04 10:41:35 -0600

    chore: fonts add to project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
      
-      <p className="text-7xl font-bold text-purple-400 underline">
+      <p className="text-3xl font-extralight text-purple-400 underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
