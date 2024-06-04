c93329b6d55efb1cd09563e7f683bef9b73b1709
brayan <brayan.mail.tester@gmail.com>
2024-06-04 10:21:19 -0600

    chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+     
+      <p className="text-7xl font-bold text-purple-400 underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
